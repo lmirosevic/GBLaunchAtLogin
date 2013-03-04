03dc582c234b9fbf30f6740a70297e3a0391ca68
Luka Mirosevic <luka@goonbee.com>
2013-03-04 15:09:12 +0100

    Initial Commit

---
+++ GBLaunchAtLogin/GBLaunchAtLogin.h
@@ -0,0 +1,13 @@
+//
+//  GBLaunchAtLogin.h
+//  GBLaunchAtLogin
+//
+//  Created by Luka Mirosevic on 04/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLaunchAtLogin : NSObject
+
+@end
---
+++ GBLaunchAtLogin/GBLaunchAtLogin.m
@@ -0,0 +1,13 @@
+//
+//  GBLaunchAtLogin.m
+//  GBLaunchAtLogin
+//
+//  Created by Luka Mirosevic on 04/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBLaunchAtLogin.h"
+
+@implementation GBLaunchAtLogin
+
+@end
